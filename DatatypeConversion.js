f642d6b0358a1aa4d48e37a18d0dadb8a5e373ec
ketan <ketankhadse14@gmail.com>
2025-04-23 19:10:20 +0530

    comparison with prefix and postfix operators

--- DatatypeConversion.js
+++ DatatypeConversion.js
@@ -20,13 +20,15 @@ let newNameString = Number(NameString);
 //console.log(typeof(newNameString));
 //console.log(newNameString);
 ///////////////////////////Operations/////////////////////////
- console.log("1"+ 1);
- console.log(1+ "1");
- console.log(1 + 1);
- console.log(1+1+"1");
- console.log("1"+1+1);
-
-
+ //console.log("1"+ 1);
+ //console.log(1+ "1");
+ //console.log(1 + 1);
+ //console.log(1+1+"1");
+ //console.log("1"+1+1);
+let x = 3;
+let y = x--;
+console.log(`x:${x} , y:${y}`);
+ 
 
 
 
