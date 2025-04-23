73d81f0455a976c6f9a7c186a00e82fc18e13036
ketan <ketankhadse14@gmail.com>
2025-04-23 17:29:30 +0530

    Datatypes

---
+++ Datatypes.js
@@ -0,0 +1,12 @@
+"use strict" //treat all js code as newer version
+ // null => standalone value
+ //Undefined => of type undefined
+ //Symbol => unique
+
+ //object
+
+let Name = "Ketan";
+ console.log(typeof Name);
+console.log(typeof null);
+console.log(typeof undefined);
+ 
