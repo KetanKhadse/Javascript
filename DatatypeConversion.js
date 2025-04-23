b93e8ce6c72ec8aac650b2ec6305ad9568b2f688
ketan <ketankhadse14@gmail.com>
2025-04-23 18:08:12 +0530

    operators

--- DatatypeConversion.js
+++ DatatypeConversion.js
@@ -1,23 +1,32 @@
 //Converting datatypes
 let score = "123";
 
-console.log(typeof score);
-console.log(typeof(score));
+//console.log(typeof score);
+//console.log(typeof(score));
 
 let NewScore = Number(score);
-console.log(typeof NewScore);
-console.log(NewScore);
+//console.log(typeof NewScore);
+//console.log(NewScore);
 score = "123abc";
 NewScore = Number(score)
-console.log(typeof NewScore);
-console.log(NewScore);
+//console.log(typeof NewScore);
+//console.log(NewScore);
 
 let NameString = "";
-console.log(typeof(NameString));
-console.log(NameString);
+//console.log(typeof(NameString));
+//console.log(NameString);
 
 let newNameString = Number(NameString);
-console.log(typeof(newNameString));
-console.log(newNameString);
+//console.log(typeof(newNameString));
+//console.log(newNameString);
+///////////////////////////Operations/////////////////////////
+ console.log("1"+ 1);
+ console.log(1+ "1");
+ console.log(1 + 1);
+ console.log(1+1+"1");
+ console.log("1"+1+1);
+
+
+
 
 
