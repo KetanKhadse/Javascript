8b72876140a4ac80c2ebb57f06825abd07dcd666
ketan <ketankhadse14@gmail.com>
2025-04-23 16:57:23 +0530

    variable file

---
+++ Variable.js
@@ -0,0 +1,7 @@
+//variable need some memory for   
+const accountId = 12345;
+var Accountname = "Ketan";
+let lastname = "khadse";
+accountcity = "pune";
+
+console.table([accountId, Accountname, lastname, accountcity])
