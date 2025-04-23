30dda8e4613abee504290f4f5186eac67bb25d8a
ketan <ketankhadse14@gmail.com>
2025-04-23 17:44:40 +0530

    datatype conversion

---
+++ DatatypeConversion.js
@@ -0,0 +1,23 @@
+//Converting datatypes
+let score = "123";
+
+console.log(typeof score);
+console.log(typeof(score));
+
+let NewScore = Number(score);
+console.log(typeof NewScore);
+console.log(NewScore);
+score = "123abc";
+NewScore = Number(score)
+console.log(typeof NewScore);
+console.log(NewScore);
+
+let NameString = "";
+console.log(typeof(NameString));
+console.log(NameString);
+
+let newNameString = Number(NameString);
+console.log(typeof(newNameString));
+console.log(newNameString);
+
+
