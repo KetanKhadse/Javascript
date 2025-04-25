05b3da8eeecc97c28f238ad1bc49f7017d44a07f
ketan <ketankhadse14@gmail.com>
2025-04-25 15:04:41 +0530

    strings

---
+++ Strings.js
@@ -0,0 +1,16 @@
+let name = "Ketan";
+const Lastname = "Khadse";
+//console.log(name + " "+Lastname); not that good 
+//console.log(`Hello My Name is ${name} ${Lastname}`);
+const GameName = new String("Ketan");
+//console.log(GameName);
+//console.log(typeof GameName);
+//console.log(GameName[0]);
+//console.log(typeof(Lastname));
+//let NewName = Lastname.toUpperCase 
+//console.log(typeof(NewName));
+let NewName = Lastname.big();
+console.log(NewName);
+
+
+
