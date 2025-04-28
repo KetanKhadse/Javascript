d212e2fc418cb72ad83328a67bdedc140e58bd9b
ketan <ketankhadse14@gmail.com>
2025-04-28 11:50:29 +0530

    destructuring

---
+++ objectJson.js
@@ -0,0 +1,11 @@
+ 
+ //Obj Destructuring
+ const Obj = {
+    CourseName:"Javascript",
+    Price: 1000
+ }
+ const {CourseName : Name} = Obj;
+ //console.log(CourseName); 
+ console.log(Name);
+ 
+
