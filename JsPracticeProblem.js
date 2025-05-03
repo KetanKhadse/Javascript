c39b7ff0a617bc4e5edaa503eaa663d7aa0505e2
ketan <ketankhadse14@gmail.com>
2025-05-03 12:13:33 +0530

    Dom prqactice

--- JsPracticeProblem.js
+++ JsPracticeProblem.js
@@ -6,8 +6,28 @@ for (let i = 1; i <=5; i++){
         let element = [i,j]; 
         row.push(element)
     }
-  console.log(row); 
+  //console.log(row); 
 }
+let string = "Racecar"; 
+let newstring = string.toLowerCase();
+let reversedString="";
+//let reversedString = newstring.split("").reverse().join("");
+// if(reversedString === newstring){
+//     console.log(`${string} is a palindrome`);
+// }
+// else{
+//     console.log(`${string} is not a palindrome`);
+    
+// }
 
-
-
+for (let i = newstring.length - 1; i >= 0; i--) {
+    reversedString += newstring[i];   
+}
+ if(reversedString === newstring){
+        console.log(`${string} is a palindrome`);
+    }
+    else{
+        console.log(`${string} is not a palindrome`);
+        
+    }
+    
